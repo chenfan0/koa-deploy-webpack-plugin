e881c71d0f41262223a861828676cc49bb545479
陈帆 <1490040662@qq.com>
2022-03-21 11:53:49 +0800

    增加配置staticConfig

--- example/cf_deploy_project/index.js
+++ example/cf_deploy_project/index.js
@@ -3,11 +3,7 @@ const static = require("koa-static-history");
 
 const app = new Koa();
 
-app.use(
-  static("./dist", {
-    history: true,
-  })
-);
+app.use(static("./dist", {}));
 
 app.listen("8080", () => {
   console.log("server run!!!");
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,14 +2,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const KoaDeployWebpackPlugin = require("../index");
 
 module.exports = {
+  output: {
+    clean: true,
+  },
   plugins: [
     new HtmlWebpackPlugin(),
     new KoaDeployWebpackPlugin({
       ssh: {
-        host: "81.71.36.158", // 你的服务器地址，例如 111.111.111.111
+        host: "", // 你的服务器地址，例如 111.111.111.111
         port: 22, // ssh连接的端口号，默认为22
         username: "root", // 连接服务器的用户名，默认为root
-        password: "c13005261761F", // 连接服务器的密码
+        password: "", // 连接服务器的密码
       },
       project: {
         name: "cf_deploy_project", // 你的项目目录名, 默认为cf_deploy_project
@@ -18,6 +21,13 @@ module.exports = {
       },
       compress: {
         test: /\.(css|js)$/i,
+      },
+      staticConfig: {
+        history: true,
+        maxage: {
+          html: 0,
+          js: 100000,
+        },
       },
     }),
   ],
