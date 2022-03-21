3fcb824416af578da11305da7cd1c588f39ce7e8
陈帆 <1490040662@qq.com>
2022-03-21 11:57:01 +0800

    增加staticConfig配置选项

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const compressionPlugin = require("compression-webpack-plugin");
 
-const { join, extend } = require("./lib/utilis");
+const { join, extend, isObject } = require("./lib/utilis");
 const { runCommand } = require("./lib/command");
 const { PLUGIN_NAME } = require("./lib/constant");
+const { defaultSshConfig, defaultProjectConfig } = require("./lib/config");
 const initialLocalProject = require("./lib/initialProject");
 
 class KoaDeployWebpackPlugin {
   constructor(config = {}) {
-    const defaultSshConfig = {
-      port: 22,
-      username: "root",
-    };
-    const defaultProjectConfig = {
-      name: "cf_deploy_project",
-      path: "/",
-      port: 8080,
-    };
-    this.sshConfig = extend(defaultSshConfig, config.ssh);
-    this.projectConfig = extend(defaultProjectConfig, config.project);
-    this.compressConfig = config.compress || {};
+    const { ssh, project, staticConfig, compress } = config;
+    this.sshConfig = extend(defaultSshConfig, ssh);
+    this.projectConfig = extend(defaultProjectConfig, project);
+    this.staticConfig = isObject(staticConfig) ? staticConfig : {};
+    this.compressConfig = isObject(compress) ? compress : {};
   }
   apply(compiler) {
     // use compression-webpack-plugin
@@ -32,7 +26,12 @@ class KoaDeployWebpackPlugin {
       const projectName = this.projectConfig.name;
       const projectPort = this.projectConfig.port;
       const localProjectPath = join(process.cwd(), projectName);
-      initialLocalProject(webpackBuildPath, localProjectPath, projectPort);
+      initialLocalProject(
+        webpackBuildPath,
+        localProjectPath,
+        projectPort,
+        this.staticConfig
+      );
       runCommand(this.sshConfig, this.projectConfig);
     });
   }
--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const static = require("koa-static-history");
 
 const app = new Koa();
 
-app.use(
-  static("./dist", {
-    history: true,
-  })
-);
+app.use(static("./dist", "staticConfig"));
 
 app.listen("port", () => {
   console.log("server run!!!");
